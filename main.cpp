e48ae486f171b70ca2deb6ddef99346d635e2589
ltch23 <luiston23@gmail.com>
2016-10-25 19:31:12 -0500

    2nd better

--- main.cpp
+++ main.cpp
@@ -8,20 +8,21 @@ int main()
 
 
 long long in;
-// cin>>in; 
+cin>>in; 
 //num=100;
 //int cont=0; int if1=0;int else1=0;
 
-cout<<"num\tcont\tdescom"<<endl;
+cout<<"num\tcont\tdescom\t\tif1\tif2\telse1\tif3"<<endl;
 
-// for(long long num=0;num<=in;num++)
-// {
-long long num=153808593750;
+ for(long long num=0;num<=in;num++)
+ {
+// long long num=153808593750;
 // long long num=10986328125;
 
 vector<int>  m_vector;
 	int i=9;
-	int cont=0; int if1=0; int if2=0;int if3=0; int if4=0;int else1=0; int else2=0;
+	int cont=0;int if1=0; int if2=0;int else1=0;int if3=0;
+
 	long long tmp=num;
 	while(tmp>1){
 		cont++;
@@ -32,15 +33,11 @@ vector<int>  m_vector;
 			tmp=tmp/i;
 			m_vector.push_back(i);
 			if2++;
-			if(tmp%i==0) {if3++;continue;}
-			else i=9,else1++;
 			}
 		else{
-			else2++;
+			else1++;
 			if(i==2){
-				cont=0;
-				m_vector.push_back(0);
-				if4++;
+				cont=0;m_vector.clear();if3++;
 				break;
 			}
 			i--;
@@ -48,17 +45,17 @@ vector<int>  m_vector;
 	}
 //cout<<'\t';
 cout<<num<<"\t"<<cont<<"\t";	
-cout<<if1<<"\t"<<if2<<"\t";	
-cout<<if3<<"\t"<<else1<<"\t";	
-cout<<if4<<"\t"<<else2<<"\t";	
 
 for (int i=m_vector.size()-1;i>=0;i--)
 	cout<< m_vector[i]<<" ";
 m_vector.clear();
+cout<<"\t\t";
+cout<<if1<<"\t"<<if2<<"\t";	
+cout<<else1<<"\t"<<if3<<"\t";	
 cout<<endl;
 
 
-// }
+}
 
 
 /*cout<<"if1: "<<if1<<endl;
